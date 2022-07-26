9299b030f509c410db13b574301aace9860460f9
obaydullah <obaydullah599@gmail.com>
2022-07-26 11:39:49 +0600

    Reserve it

---
+++ ReserveIt/Controllers/WordListController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReserveIt.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class WordListController : ControllerBase
+   {
+      private static readonly List<string> allwords = new List<string>
+      {
+         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+      };
+      [HttpGet]
+      public async Task<IActionResult> GET(int n)
+      {
+         if (allwords.Count > 0)
+         {
+            var st = string.Join(" ", allwords);
+            var words = st.ToCharArray();
+            var result = "";
+            var counter = 0;
+            foreach (var word in words)
+            {
+
+               if (counter >= n)
+               {
+                  result += "\n";
+                  result += word;
+                  counter = 0;
+               }
+               else
+               {
+                  result += word;
+                  counter++;
+               }
+            }
+            return Ok(result);
+
+         }
+         else
+         {
+            allwords.Add("First");
+            return Ok(allwords);
+         }
+
+
+
+      }
+      [HttpPost]
+      public IActionResult Post([FromBody] IEnumerable<string> items)
+      {
+
+         allwords.AddRange(items);
+
+         string i = "";
+
+         if (Request.Headers.TryGetValue("page-size", out var pz))
+         {
+            //use testId value
+            i = pz;
+         }
+
+         return Created("", pz);
+      }
+   }
+}
---
+++ ReserveIt/Models/ModelsDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ReserveIt.Models
+{
+    public class ModelsDB : DbContext
+    {
+        public ModelsDB(DbContextOptions<ModelsDB> options) : base(options)
+
+        {
+
+        }
+        public DbSet<MyMessages> MyMessages { get; set; }
+    }
+    public class MyMessages
+    {
+        public int Id { get; set; }
+        public string Words { get; set; }
+    }
+}
